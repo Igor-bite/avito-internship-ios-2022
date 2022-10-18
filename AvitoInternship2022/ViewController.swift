f1b15473cdcc0fc015e7b2d340fdeac44c759b08
Igor <klyuzhevigor@gmail.com>
2022-10-18 17:56:07 +0300

    Initial Commit

---
+++ AvitoInternship2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AvitoInternship2022
+//
+//  Created by Игорь Клюжев on 18.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
