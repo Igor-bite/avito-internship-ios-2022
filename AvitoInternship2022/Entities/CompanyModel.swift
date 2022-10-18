b718815098f5250ff9fe3245130c97190c018f3f
Igor <klyuzhevigor@gmail.com>
2022-10-18 23:31:03 +0300

    new: Added model for Company

---
+++ AvitoInternship2022/Entities/CompanyModel.swift
@@ -0,0 +1,19 @@
+//
+//  CompanyModel.swift
+//  AvitoInternship2022
+//
+//  Created by Игорь Клюжев on 18.10.2022.
+//
+
+import Foundation
+
+struct Company: Codable {
+    struct Employee: Codable {
+        let name: String
+        let phoneNumber: String
+        let skills: [String]
+    }
+
+    let name: String
+    let employees: [Employee]
+}
