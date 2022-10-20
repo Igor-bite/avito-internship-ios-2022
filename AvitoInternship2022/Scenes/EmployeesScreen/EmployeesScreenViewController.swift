ef4a8ebc6779809f508d5fb3674596ff0717e362
Igor <klyuzhevigor@gmail.com>
2022-10-20 23:20:47 +0300

    new: Added collection view

--- AvitoInternship2022/Scenes/EmployeesScreen/EmployeesScreenViewController.swift
+++ AvitoInternship2022/Scenes/EmployeesScreen/EmployeesScreenViewController.swift
@@ -12,15 +12,83 @@ final class EmployeesScreenViewController: UIViewController {
 
     var presenter: EmployeesScreenPresenterInterface?
 
+    // MARK: - Private properties -
+
+    private lazy var dataSource = makeDataSource()
+
+    private lazy var collectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(cellType: EmployeeCell.self)
+        collectionView.allowsSelection = false
+
+        return collectionView
+    }()
+
     // MARK: - Lifecycle -
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         navigationController?.isNavigationBarHidden = true
+
+        setupViews()
+        presenter?.fetchData()
+    }
+
+    private func setupViews() {
+        view.addSubview(collectionView)
+        collectionView.dataSource = dataSource
+        let constraints = collectionViewConstraints()
+        NSLayoutConstraint.activate(constraints)
+    }
+
+    private func collectionViewConstraints() -> [NSLayoutConstraint] {
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return [
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ]
+    }
+
+    private func makeDataSource() -> UICollectionViewDiffableDataSource<EmployeesScreenSection, Company.Employee> {
+        let dataSource = UICollectionViewDiffableDataSource<EmployeesScreenSection, Company.Employee>(
+            collectionView: collectionView
+        ) { collectionView, indexPath, employee -> UICollectionViewCell? in
+
+            let cell: EmployeeCell? = collectionView.dequeueReusableCell(for: indexPath)
+            if let cell = cell {
+                cell.configure(employee: employee)
+                return cell
+            }
+
+            return .init()
+        }
+
+        return dataSource
     }
 }
 
 // MARK: - Extensions -
 
-extension EmployeesScreenViewController: EmployeesScreenViewInterface {}
+extension EmployeesScreenViewController: EmployeesScreenViewInterface {
+    func reloadData() {
+        guard let presenter = presenter
+        else {
+            assertionFailure("Data source for collection view is missing")
+            return
+        }
+        var snapshot = NSDiffableDataSourceSnapshot<EmployeesScreenSection, Company.Employee>()
+        let activeSections = presenter.activeSections
+        snapshot.appendSections(activeSections)
+        activeSections.forEach { section in
+            snapshot.appendItems(presenter.items(forSection: section), toSection: section)
+        }
+
+        dataSource.apply(snapshot, animatingDifferences: true)
+    }
+}
