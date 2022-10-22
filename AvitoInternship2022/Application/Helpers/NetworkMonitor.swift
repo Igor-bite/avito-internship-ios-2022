77bb8dc4507bddb53079365573fa7e18ca8c1094
Igor <klyuzhevigor@gmail.com>
2022-10-23 00:24:05 +0300

    fix: Upgraded network monitor

--- AvitoInternship2022/Application/Helpers/NetworkMonitor.swift
+++ AvitoInternship2022/Application/Helpers/NetworkMonitor.swift
@@ -18,7 +18,7 @@ final class NetworkMonitor {
     private let queue = DispatchQueue(label: "NetworkConnectivityMonitor")
     private let monitor: NWPathMonitor
 
-    private(set) var isConnected = false
+    private(set) lazy var isConnected: Bool = isConnected(withPath: monitor.currentPath)
 
     private init() {
         monitor = NWPathMonitor()
@@ -26,13 +26,21 @@ final class NetworkMonitor {
 
     func startMonitoring() {
         monitor.pathUpdateHandler = { [weak self] path in
-            self?.isConnected = path.status != .unsatisfied
+            guard let self = self else { return }
+            let isConnected = self.isConnected(withPath: path)
+            guard isConnected != self.isConnected else { return }
+
             NotificationCenter.default.post(name: .connectivityStatus, object: nil)
+            self.isConnected = isConnected
         }
         monitor.start(queue: queue)
     }
 
     func stopMonitoring() {
         monitor.cancel()
+    }
+
+    private func isConnected(withPath path: NWPath) -> Bool {
+        path.status != .unsatisfied
     }
 }
